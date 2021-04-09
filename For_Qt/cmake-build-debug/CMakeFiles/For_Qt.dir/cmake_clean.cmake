d3311128226d8fa2a51d74346454c2992f85f368
Vova05 <63654169+Vova05@users.noreply.github.com>
2021-04-10 02:07:06 +0300

    Add files via upload

---
+++ For_Qt/cmake-build-debug/CMakeFiles/For_Qt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/For_Qt.dir/main.cpp.obj"
+  "For_Qt.exe"
+  "For_Qt.exe.manifest"
+  "For_Qt.pdb"
+  "libFor_Qt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/For_Qt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
